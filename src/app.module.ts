7aaf82627f35ce3e505bb35b5054805c9ea21c30
sonchaegeon <thscorjs1002@naver.com>
2021-05-09 22:02:06 +0900

    [UPDATE] import database config, notice module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { APP_FILTER } from '@nestjs/core';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { HttpErrorFilter } from './common/exception/exception.filter';
+import { connectionOptions } from './config';
+import { NoticeModule } from './notice/notice.module';
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      load: [connectionOptions],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => config.get(process.env.NODE_ENV),
+      inject: [ConfigService],
+    }),
+    NoticeModule,
+  ],
   providers: [
     {
       provide: APP_FILTER,
