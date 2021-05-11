58fa9274dce9e35c5392dca88dcb94a16b590bb1
sonchaegeon <thscorjs1002@naver.com>
2021-05-11 22:58:18 +0900

    [UPDATE] app module set global
    provide jwt strategy
    import notice module, jwt module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { APP_FILTER } from '@nestjs/core';
+import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { HttpErrorFilter } from './common/exception/exception.filter';
+import { JwtStrategy } from './common/strategy/jwt.strategy';
 import { connectionOptions } from './config';
 import { NoticeModule } from './notice/notice.module';
 
+@Global()
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -16,9 +19,11 @@ import { NoticeModule } from './notice/notice.module';
       useFactory: (config: ConfigService) => config.get(process.env.NODE_ENV),
       inject: [ConfigService],
     }),
+    JwtModule.register({}),
     NoticeModule,
   ],
   providers: [
+    JwtStrategy,
     {
       provide: APP_FILTER,
       useClass: HttpErrorFilter,
