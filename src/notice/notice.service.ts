edf23f5fd06204aa41892e31a78a0f8c50724b86
sonchaegeon <thscorjs1002@naver.com>
2021-05-11 23:03:36 +0900

    [UPDATE] notice upload file service

--- src/notice/notice.service.ts
+++ src/notice/notice.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { NoticeFile } from './entity/notice-file.entity';
+import { NoticeFileRepository } from './entity/notice-file.repository';
 
 @Injectable()
-export class NoticeService {}
+export class NoticeService {
+  constructor(
+    @InjectRepository(NoticeFile)
+    private readonly noticeFileRepository: NoticeFileRepository,
+  ) {}
+
+  public async uploadFile(filename: string, id: number): Promise<NoticeFile> {
+    return await this.noticeFileRepository.uploadFile(filename, id);
+  }
+}
