bfea3c9bcb6a9996e7d1d20a6eb0825d97ed10dd
sonchaegeon <thscorjs1002@naver.com>
2021-05-12 14:25:47 +0900

    [UPDATE] add file not found exception

--- src/common/exception/exception.index.ts
+++ src/common/exception/exception.index.ts
@@ -1,5 +1,7 @@
-import { UnauthorizedException } from '@nestjs/common';
+import { NotFoundException, UnauthorizedException } from '@nestjs/common';
 
 export const unauthorizedTokenException = new UnauthorizedException(
   'Unauthorized Token',
 );
+
+export const FileNotFoundException = new NotFoundException('File Not Found');
