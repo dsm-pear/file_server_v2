5d7d6ea414d848c67557850354f63f6247073e58
sonchaegeon <thscorjs1002@naver.com>
2021-05-15 18:35:36 +0900

    [UPDATE] notice modify file repository

--- src/notice/entity/notice-file.repository.ts
+++ src/notice/entity/notice-file.repository.ts
@@ -1,14 +1,22 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { EntityRepository, Repository, UpdateResult } from 'typeorm';
 import { NoticeFile } from './notice-file.entity';
 
 @EntityRepository(NoticeFile)
 export class NoticeFileRepository extends Repository<NoticeFile> {
-  public async uploadFile(filename: string, id: number) {
+  public async uploadFile(filename: string, id: number): Promise<NoticeFile> {
     let newNoticeFile: NoticeFile;
     newNoticeFile = this.create({
       path: filename,
       notice_id: id,
     });
     return await this.save(newNoticeFile);
+  }
+
+  public async modifyFile(
+    filename: string,
+    noticeFile: NoticeFile,
+  ): Promise<NoticeFile> {
+    noticeFile.path = filename;
+    return await this.save(noticeFile);
   }
 }
