a301b7422f9336c6dc9e113c4bf90083c0875a23
sonchaegeon <thscorjs1002@naver.com>
2021-05-18 16:48:34 +0900

    [UPDATE] add admin jwt strategy

--- src/common/strategy/jwt.strategy.ts
+++ src/common/strategy/jwt.strategy.ts
@@ -2,7 +2,10 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { unauthorizedTokenException } from '../exception/exception.index';
+import {
+  adminForbiddenException,
+  unauthorizedTokenException,
+} from '../exception/exception.index';
 import { TokenPayload } from '../interface/payload.interface';
 
 @Injectable()
@@ -18,6 +21,29 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   async validate(payload: TokenPayload): Promise<{ sub: string }> {
     if (payload.type !== 'access') {
       throw unauthorizedTokenException;
+    }
+
+    return { sub: payload.sub };
+  }
+}
+
+@Injectable()
+export class AdminJwtStrategy extends PassportStrategy(Strategy, 'admin-jwt') {
+  constructor(configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get('JWT_SECRET'),
+    });
+  }
+
+  async validate(payload: TokenPayload): Promise<{ sub: string }> {
+    if (payload.type !== 'access') {
+      throw unauthorizedTokenException;
+    }
+
+    if (payload.role == 'user') {
+      throw adminForbiddenException;
     }
 
     return { sub: payload.sub };
