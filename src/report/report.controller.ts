14f0f0056da0f5a1d5840a7ba414a46a45c7a988
sonchaegeon <thscorjs1002@naver.com>
2021-05-19 18:12:49 +0900

    [UPDATE] upload report file controller

--- src/report/report.controller.ts
+++ src/report/report.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { JwtAuthGuard } from 'src/common/guard/jwt-auth.guard';
+import { MulterConfigs } from 'src/config/multer';
+import { ReportFile } from './entity/report-file.entity';
+import { ReportService } from './report.service';
 
 @Controller('report')
-export class ReportController {}
+export class ReportController {
+  constructor(private reportService: ReportService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @UseInterceptors(FileInterceptor('reportFile', MulterConfigs))
+  @Post('file/:report_id')
+  public async uploadFile(
+    @UploadedFile() file: Express.Multer.File,
+    @Param('report_id') id: number,
+  ): Promise<ReportFile> {
+    return await this.reportService.uploadFile(file.filename, id);
+  }
+}
