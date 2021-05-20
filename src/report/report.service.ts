d552d271b5209049adf34f50c3fe07d7d94c8599
sonchaegeon <thscorjs1002@naver.com>
2021-05-21 00:34:39 +0900

    [UPDATE] modify report file service

--- src/report/report.service.ts
+++ src/report/report.service.ts
@@ -16,7 +16,7 @@ export class ReportService {
   }
 
   public async getReportFiles(id: number): Promise<ReportFile[]> {
-    const reportFiles = await this.reportRepository.find({ report_id: id });
+    const reportFiles = await this.reportRepository.find({ report: id });
     if (reportFiles.length === 0) throw FileNotFoundException;
 
     return reportFiles;
@@ -28,8 +28,14 @@ export class ReportService {
     return reportFile.path;
   }
 
+  public async modifyFile(filename: string, id: number): Promise<ReportFile> {
+    const reportFile = await this.isExistFile(id);
+    return await this.reportRepository.modifyFile(filename, reportFile);
+  }
+
   private async isExistFile(id: number): Promise<ReportFile> {
     const reportFile = await this.reportRepository.findOne({ id });
+    console.log(reportFile);
     if (!reportFile) throw FileNotFoundException;
     return reportFile;
   }
