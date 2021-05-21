5f8df561453b79f971556e5e611ce6dd251e12a4
sonchaegeon <thscorjs1002@naver.com>
2021-05-21 11:57:22 +0900

    [UPDATE] one to many with member, one to one with report file
    relation

--- src/common/entity/report/report.entity.ts
+++ src/common/entity/report/report.entity.ts
@@ -1,9 +1,13 @@
+import { ReportFile } from 'src/report/entity/report-file.entity';
 import {
   Column,
   CreateDateColumn,
   Entity,
+  OneToMany,
+  OneToOne,
   PrimaryGeneratedColumn,
 } from 'typeorm';
+import { Member } from '../member/member.entity';
 
 @Entity('report_tbl')
 export class Report {
@@ -25,12 +29,18 @@ export class Report {
   @Column({ type: 'tinyint' })
   is_submitted: number;
 
-  @Column({ length: 100 })
+  @Column({ length: 100, nullable: true })
   comment: string;
 
-  @Column({ length: 100 })
+  @Column({ length: 100, nullable: true })
   github: string;
 
-  @Column({ length: 100 })
+  @Column({ length: 100, nullable: true })
   team_name: string;
+
+  @OneToMany(() => Member, (member) => member.report)
+  members: Member[];
+
+  @OneToOne(() => ReportFile, (reportFile) => reportFile.report)
+  reportFile: ReportFile;
 }
