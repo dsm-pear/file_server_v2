5665664ad220f4f62322f9e8d94bc7d3b395ad34
sonchaegeon <thscorjs1002@naver.com>
2021-05-21 11:59:36 +0900

    [UPDATE] imports report repository, member repository

--- src/report/report.module.ts
+++ src/report/report.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { MemberRepository } from 'src/common/entity/member/member.repository';
+import { ReportRepository } from 'src/common/entity/report/report.repository';
 import { ReportFileRepository } from './entity/report-file.repository';
 import { ReportController } from './report.controller';
 import { ReportService } from './report.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ReportFileRepository])],
+  imports: [
+    TypeOrmModule.forFeature([
+      ReportFileRepository,
+      ReportRepository,
+      MemberRepository,
+    ]),
+  ],
   controllers: [ReportController],
   providers: [ReportService],
 })
