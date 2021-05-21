ce8f1b28d743f2ee34b677c102ec5ede5ba5b84a
sonchaegeon <thscorjs1002@naver.com>
2021-05-21 11:58:27 +0900

    [UPDATE] add adminForbidden Exception and ReportNotFound

--- src/common/exception/exception.index.ts
+++ src/common/exception/exception.index.ts
@@ -8,6 +8,12 @@ export const unauthorizedTokenException = new UnauthorizedException(
   'Unauthorized Token',
 );
 
-export const FileNotFoundException = new NotFoundException('File Not Found');
-
 export const adminForbiddenException = new ForbiddenException('Forbidden');
+export const userForbiddenException = new ForbiddenException(
+  'Not allowed to access file',
+);
+
+export const FileNotFoundException = new NotFoundException('File Not Found');
+export const ReportNotFoundException = new NotFoundException(
+  'Report Not Found',
+);
